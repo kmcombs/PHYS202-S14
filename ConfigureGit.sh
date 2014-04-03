721bcac07f25e68a69445d8bb7ca496a14cab64e
Katherine Combs <kmcombs@calpoly.edu>
2014-04-03 15:02:59 -0700

    track Config script and pledge

---
+++ ConfigureGit.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git config --global  user.name "Katherine Combs" 
+git config --global user.email "kmcombs@calpoly.edu"
+git config --global color.ui "auto"
+git config --global core.editor "nano"
